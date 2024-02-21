8cb9d65bf1fa64f9656b3fea0acc96ff1edde585
rKmasterworks <32278144+rKmasterworks@users.noreply.github.com>
2024-02-21 08:59:41 +0100

    Did practice on JS, working on task now

---
+++ testing.js
@@ -0,0 +1,33 @@
+console.log("Hello, world!")
+
+let message
+message = "hello"
+console.log(message)
+
+let message1 = "hello1"
+console.log(message1)
+
+let user = "John", age = 25, message2 = "hello2" //Don't do this
+
+let user1 = "John";
+age = 25;
+message3 = "hello3"; //do this
+
+let message4;
+message4 = 'Hello!';
+message4 = 'World!'; // value changed
+console.log(message4);
+
+let hello = 'Hello world!';
+let message5;
+// copy 'Hello world' from hello into message
+message5 = hello;
+// now two variables hold the same data
+console.log(hello);
+console.log(message5);
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+console.log($ + _);
+
+const myBirthDay = "11.01.2007" //Cannot be changed
