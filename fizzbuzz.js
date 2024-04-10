faee7dc1b6d029b1fb7efeeda270dd6f4916336a
rKmasterworks <32278144+rKmasterworks@users.noreply.github.com>
2024-04-10 09:06:09 +0200

    Tasks on The Odin Project

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+let answer = parseInt(prompt("Enter the number you want to FizzBuzz up to:"))
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+  }
---
+++ strings.js
@@ -0,0 +1,17 @@
+const string = "The revolution will not be televised"
+console.log(string)
+
+const badString = string;
+console.log(badString);
+
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting);
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined);
+
+
+
